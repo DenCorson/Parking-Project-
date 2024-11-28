1c3d10b0c4dadee1b20d48e46b58f961562acb83
DenCorson <166075054+DenCorson@users.noreply.github.com>
2024-11-28 01:39:12 -0800

    Update ParkingLot.hpp

--- ParkingLot.hpp
+++ ParkingLot.hpp
@@ -7,82 +7,82 @@
 #include <iostream>
 using namespace std;
 
-class ParkingLot : public Parking{
+class ParkingLot : public Parking{ 
 
-std:: vector<ParkingSpace> space;
-string lotName;
+std:: vector<ParkingSpace> space; // vector of lots will contain objects of spaces from ParkingSpace class
+string lotName; // name of the lot(section)
 
 public:
 
 ParkingLot(const string& name)
 {
-    lotName = name;
+    lotName = name; // name assigned to lotname variable
 }
 
 void addSpace(ParkingSpace& xspace)
 {
-    space.push_back(xspace);
+    space.push_back(xspace);  // add space takes in space object into parameter and pushes onto space vector
 }
 
-vector<ParkingSpace>& getSpaces()
+vector<ParkingSpace>& getSpaces() // retrieves spaces in vector
 {
     return space;
 }
 
-vector<ParkingSpace>& getOccupiedSpaces()
+vector<ParkingSpace>& getOccupiedSpaces()  // returns spaces which are occupied in vector
 {
-    vector<ParkingSpace> occupied;
-    for (int i = 0; i < space.size(); i++)
+    vector<ParkingSpace> occupied; // local vector "occupied" created to hold tracked occupied spaces
+    for (int i = 0; i < space.size(); i++) // iterators for space vector
     {
-        if (!space[i].getIsSpaceEmpty())
+        if (!space[i].getIsSpaceEmpty()) // if condition uses getIsSpaceEmpty function to verify that its NOT empty
         {
-            occupied.push_back(space[i]);
+            occupied.push_back(space[i]); // if grabs occupied spaes and pushes it onto the occupied vector to later be displayed back to user
         }
     }
-    return occupied;
+    return occupied; // returns the object of occupied spaces in the space vector
 }
   
-    void display() override 
+    void display() override  // diplay information regarding the parking lot
     {
-        cout << "Displaying parking lot: " << lotName << endl;
-        for (int i = 0; i < space.size(); i++) 
+        cout << "Displaying parking lot: " << lotName << endl; //       
+        for (int i = 0; i < space.size(); i++)  // iterates through "lot" vector and displays information on each lot 
         {
             space[i].display();
         }
     }
 
-    int countEmptySpaces()
+    int countEmptySpaces() // counts  the number emptypaces in the lot
     {
-        int emptySpots = 0;
+        int emptySpace = 0;
         for (int i = 0; i < space.size(); i++)
             {
                 if(space[i].getIsSpaceEmpty())
                 {
-                    emptySpots++;
+                    emptySpace++;
                 }
             }
-            return emptySpots;
+            return emptySpace;
     }
 
-    int countFullSpaces()
+    int countFullSpaces() // counts  the number of full spaces in the lot
     {
-        int occupiedSpots = 0;
+        int occupiedSpaces = 0;
         for(int i = 0; i <space.size(); i++)
         {
             if(space[i].getIsSpaceEmpty())
             {
-                occupiedSpots++;
+                occupiedSpaces++;
             }
         }
-        return occupiedSpots;
+        return occupiedSpaces;
     }
 
-    bool reserveSpace(int spaceNumber) 
+    bool reserveSpace(int spaceNumber)  // reserves space, with the space number
     {
-        for (int i =0; i < space.size(); i++)
+        for (int i =0; i < space.size(); i++)  // iterates through lot vector of spaces
         {
-            if (space[i].getSpaceNumber() == spaceNumber && space[i].getIsSpaceEmpty())
-            {
+            if (space[i].getSpaceNumber() == spaceNumber && space[i].getIsSpaceEmpty()) //condition checks if the entered space number is empty if so, reserve by setting space to occupied
+             {
                 space[i].setSpacetoFull();
                 cout << "Space #" << spaceNumber << " has been reserved." << endl;
                 return true;
@@ -90,11 +90,11 @@ vector<ParkingSpace>& getOccupiedSpaces()
         }
     }
 
-    bool clearSpace(int spaceNumber)
+    bool clearSpace(int spaceNumber) //clears the reserve spot with number in spot
     {
-         for (int i =0; i < space.size(); i++)
+         for (int i =0; i < space.size(); i++) //iterates through lot vector of spaces
         {
-            if (space[i].getSpaceNumber() == spaceNumber && !space[i].getIsSpaceEmpty())
+            if (space[i].getSpaceNumber() == spaceNumber && !space[i].getIsSpaceEmpty()) // condition checks if the entered space number is not empty, if true set space to empty
             {
                 space[i].setSpacetoEmpty();
                 cout << "Space #" << spaceNumber << " is now free." << endl;
@@ -103,10 +103,10 @@ vector<ParkingSpace>& getOccupiedSpaces()
         }
     }
 
-    string getName() const override 
-    {
-        return lotName;
-    }
+    //string getName() const override 
+   // {
+     //   return lotName;
+    //}
 
 
 };
